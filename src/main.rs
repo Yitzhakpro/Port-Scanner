5153c2c3d51ab357bebfb9273c9975a634e9a936
Yitzhakpro <isako14@gmail.com>
2023-04-29 15:18:39 +0300

    added verbose logs

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,28 @@ extern crate core;
 mod network;
 mod scanner_args;
 
-use clap::Parser;
-use network::is_port_up;
-use scanner_args::PortScannerArgs;
 use std::sync::mpsc;
 use std::thread;
-use crate::network::get_socket_addr;
+use clap::Parser;
+use network::{get_socket_addr, is_port_up};
+use scanner_args::PortScannerArgs;
+
 
 fn main() {
     let cli = PortScannerArgs::parse();
     let ip = cli.ip;
     let ports = cli.ports;
     let udp = cli.udp;
+    let output = cli.output;
+    let verbose = cli.verbose;
 
-    let (tx, rx): (mpsc::Sender<u16>, mpsc::Receiver<u16>) = mpsc::channel();
+    let mut open_ports: Vec<u16> = Vec::new();
+
+    let (tx, rx): (mpsc::Sender<(u16, bool)>, mpsc::Receiver<(u16, bool)>) = mpsc::channel();
+
+    if verbose {
+        println!("Starting port scan on: {}", ip);
+    }
 
     for port in ports.iter() {
         let tx = tx.clone();
@@ -27,17 +35,30 @@ fn main() {
 
         let _handle: thread::JoinHandle<()> = thread::spawn(move || {
             let is_up = is_port_up(socket_addr, 5, udp);
-            if is_up {
-                println!("port {} is up on host: {}", port_clone, ip);
-            } else {
-                println!("port {} is closed on host: {}", port_clone, ip);
+            if is_up && verbose {
+                println!("Port {} is open on {}", port_clone, ip);
             }
 
-            tx.send(port_clone).unwrap();
+            tx.send((port_clone, is_up)).unwrap();
         });
     }
 
+    // collecting opened ports from threads
     for _ in ports.iter() {
-        rx.recv().unwrap();
+        if let Ok((port, is_up)) = rx.recv() {
+            if is_up {
+                open_ports.push(port);
+            }
+        }
+    }
+
+    if verbose {
+        println!("Finished port scanning on: {}", ip);
+        println!("Creating output file in: {}", output)
+    }
+
+    // TODO: create a result file
+    for port in open_ports.iter() {
+        println!("Port {} is open on {}", port, ip);
     }
 }
